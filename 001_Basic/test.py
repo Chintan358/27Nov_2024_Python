870dee0694276684f504b6d7fd97a42fcdd0a026
chintan <chintan.tops@gmail.com>
2024-11-29 11:48:49 +0530

    29-11

--- 001_Basic/test.py
+++ 001_Basic/test.py
@@ -1,1 +1,16 @@
-print("Hello python");
+# print("Hello \" python")
+# print('Hello \' tops')
+
+# print("Hello ' python")
+# print('Hello "tops" ')
+print("""Hello\b python""")
+
+# print("Hello",end="")
+# print("python")
+
+# print("Python","java","php","node","html",sep=" | ")
+
+
+
+
+
---
+++ 001_Basic/variables.py
@@ -0,0 +1,18 @@
+
+a = 10
+b = "Tops"
+c = 10.31
+d = 10 + 4j
+e = [10,20,30,40,50]
+f = (10,20,30,40,50)
+g = {10,20,30,40,50}
+h = dict({10:"Python"})
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
