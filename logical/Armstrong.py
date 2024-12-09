3e16e5d7ca194745d8e7ddd583b0c98652d8cb1f
chintan <chintan.tops@gmail.com>
2024-12-09 11:36:22 +0530

    all

---
+++ logical/Armstrong.py
@@ -0,0 +1,30 @@
+
+a  =int(input("Enter nmber to start : "))
+b = int(input("Enter number to end : "))
+for i in range(a,b):
+############countr#################
+    # number = int(input("enter number : "))
+    number = i
+    temp = number
+    count = 0
+    while number !=0:
+        rem = number%10
+        number= int(number/10)
+        count = count+1
+
+
+    number = temp
+
+    ##############armstrong##################
+    sum =0
+    while number != 0 : 
+        rem = number%10
+        sum = sum + (rem**count)
+        number =int(number/10)
+    
+
+    if temp==sum:
+        print(f"{temp} is armstrong")
+    else :
+        pass
+        # print(f"{temp} is Not armstrong")
