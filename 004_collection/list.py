ff3720abd6cd24c7226d90dbe4b0724b4a06cb4e
chintan <chintan.tops@gmail.com>
2024-12-25 14:24:22 +0530

    all

--- 004_collection/list.py
+++ 004_collection/list.py
@@ -16,7 +16,7 @@ mix = ["Tops",1,1.23,True]
 # print(min(id))
 
 # subject.append("Node")
-# subject.extend("Node")
+# subject.extend(id)
 # print(subject)
 
 # subject.insert(1,"Node")
@@ -26,5 +26,38 @@ mix = ["Tops",1,1.23,True]
 
 # print(subject.index("React"))
 
-subject.pop()
+# subject.pop()
+# print(subject)
+
+# print(type(subject))
+
+
+# sports = list(("Cricket","Kabbaddi","Football"))
+# sports.append("Hockey")
+# print(sports)
+
+# subject[0] = "node";
+# print(subject)
+
+# del subject[0]
+# del subject
+# subject.clear()
+# print(subject)
+
+# for i in range(len(subject)):
+#     print(subject[i])
+
+# [print(i) for i in subject]
+newlist = []
+# for i in subject:
+#     if "A" in i:
+#         newlist.append(i)
+
+
+# newlist = [x for x in subject if "a" in x]
+
+# print(newlist)
+
+# subject.sort(reverse=True)
+# subject.reverse()
 print(subject)
