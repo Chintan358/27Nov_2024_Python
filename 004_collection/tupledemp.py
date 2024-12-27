a5c2981347b0e02264c4781436edb56d6b128fb9
chintan <chintan.tops@gmail.com>
2024-12-27 16:58:21 +0530

    tuple

---
+++ 004_collection/tupledemp.py
@@ -0,0 +1,49 @@
+
+#tuple
+t = ("Ankit","Khadija","Nisha","Rushabh","Ankit")
+# print(t)
+# print(len(t))
+
+# tp = ("Hello",)
+# print(tp)
+# print(type(tp))
+
+# tp1 = tuple(("A","B"))
+# print(tp1)
+
+# access tuple
+
+# for i in t:
+#     print(i)
+
+# print(t[0])
+
+# for i in range(len(t)):
+#     print(t[i])
+
+# print(t[-2])
+
+# print(t[2:4])
+
+# print(t[-4:-2])
+
+# l = list(t)
+# # print(l)
+# l.append("Sarfarz")
+# print(tuple(l))
+
+# tp = ("Kuldeep",)
+
+# t = t+tp
+
+# print(t)
+
+# (a,b,c,d,e) = t
+# (a,*b,c) = t
+
+# print(b)
+
+# tp = t * 3
+# print(tp)
+
+# print(dir(list))
