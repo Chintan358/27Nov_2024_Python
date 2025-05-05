85c164f912add30e4053f0a5209cc3c2a9248859
chintan <chintan@gmail.com>
2025-05-05 18:30:41 +0530

    all

---
+++ ecomm/myapp/urls.py
@@ -0,0 +1,34 @@
+
+from django.urls import path
+from myapp.views import *
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+  
+    path("",index,name="index"),
+    path("accounts",accounts,name="accounts"),
+    path("cart",cart,name="cart"),
+    path("checkout",checkout,name="checkout"),
+    path("details",details,name="details"),
+    path("shop",shop,name="shop"),
+    path("loginregister",loginregister,name="loginregister"),
+
+    path("registeruser",registeruser,name="registeruser"),
+    path("loginuser",loginuser,name="loginuser"),
+    path("logoutuser",logoutuser,name="logoutuser"),
+    path("addtocart",addtocart,name="addtocart"),
+    path("removecart",removecart,name="removecart"),
+    path("changeqty",changeqty,name="changeqty"),
+    path("addadr",addadr,name="addadr"),
+    path("viewadr",viewadr,name="viewadr"),
+    path("pay",pay,name="pay"),
+    path("orderplaced",orderplaced,name="orderplaced"),
+
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
