802119ce1236bbb2ed195d16990c874b74afebb2
chintan <chintan@gmail.com>
2025-05-23 12:32:57 +0530

    all

---
+++ Project/myapp/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from myapp.views import *
+
+urlpatterns = [
+    path("",index,name="index"),
+    path("cart",cart,name="cart"),
+    path("checkout",checkout,name="checkout"),
+    path("contact",contact,name="contact"),
+    path("details",details,name="details"),
+    path("shop",shop,name="shop"),
+]
